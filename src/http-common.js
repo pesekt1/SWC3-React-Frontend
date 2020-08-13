252efb94e01cbb9f4fe0812199e28e3dd4b0596f
Tomas Pesek <pesekt@gmail.com>
2020-08-13 10:57:16 +0200

    changed web server url

--- src/http-common.js
+++ src/http-common.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:5557/api",
+  //baseURL: "http://localhost:5557/api",
+  baseURL: "https://swc3-spring-boot-server.herokuapp.com/api",
   headers: {
     "Content-type": "application/json",
   },
