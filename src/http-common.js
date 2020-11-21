7f552c69f907f12ee5dbeabc4bcaf11311d617ae
pesekt1 <58634005+pesekt1@users.noreply.github.com>
2020-11-22 00:48:11 +0100

    intelliJ project files

--- src/http-common.js
+++ src/http-common.js
@@ -1,8 +1,9 @@
 import axios from "axios";
 
 export default axios.create({
-  //baseURL: "http://localhost:5557/api",
-  baseURL: "https://swc3-spring-boot-server.herokuapp.com/api",
+  //this should be improved: environment variable - DEV is localhost, PROD is cloud
+  baseURL: "http://localhost:5557/api",
+  //baseURL: "https://swc3-spring-boot-server.herokuapp.com/api",
   headers: {
     "Content-type": "application/json",
   },
