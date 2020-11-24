78f6f972d540fb5955964fbe8e0acbfa965dcf4d
pesekt1 <58634005+pesekt1@users.noreply.github.com>
2020-11-24 16:50:59 +0100

    -

--- src/components/tutorials-list.component.js
+++ src/components/tutorials-list.component.js
@@ -148,7 +148,7 @@ export default class TutorialsList extends Component {
               <div>
                 <label>
                   <strong>Description:</strong>
-                </label>{" "}
+                </label>{"    "}
                 {currentTutorial.description}
               </div>
               <div>
--- src/http-common.js
+++ src/http-common.js
@@ -2,8 +2,9 @@ import axios from "axios";
 
 export default axios.create({
   //this should be improved: environment variable - DEV is localhost, PROD is cloud
-  baseURL: "http://localhost:5557/api",
+  //baseURL: "http://localhost:5557/api",
   //baseURL: "https://swc3-spring-boot-server.herokuapp.com/api",
+  baseURL: process.env.REACT_APP_API_URL,
   headers: {
     "Content-type": "application/json",
   },
